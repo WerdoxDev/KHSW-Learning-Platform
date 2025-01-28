893613d2d50aee7c027bd3732b3605cc4f042050
Matin Tat <matin.tat85@gmail.com>
2025-01-28 11:04:44 +0100

    feat(shared): add course api types

--- packages/shared/src/api-types.ts
+++ packages/shared/src/api-types.ts
@@ -1,11 +1,16 @@
 import type { Snowflake } from "./snowflake";
 
+export type TokenPayload = { id: Snowflake; permissions: number };
+
 export type APIUser = {
 	id: Snowflake;
 	username: string;
 	email: string;
 	password?: string | null;
+	permissions: number;
 };
+
+export type APIPublicUser = Pick<APIUser, "id" | "username">;
 
 export type Tokens = { accessToken: string; refreshToken: string };
 
@@ -26,3 +31,18 @@ export type APIPostRegisterResult = APIUser & Tokens;
 
 export type APIPostRefreshTokenResult = Tokens;
 export type APIGetCurrentUserResult = APIUser;
+
+export type APICourse = {
+	id: Snowflake;
+	name: string;
+	imageUrl: string;
+	author: APIPublicUser;
+};
+
+export type APIPostCourseResult = APICourse;
+export type APIPostCourseBody = {
+	name: string;
+	imageUrl: string;
+};
+
+export type APIGetCoursesResult = APICourse[];
